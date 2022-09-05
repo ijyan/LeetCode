78e692db4762a6a0edf7a0132ec1e0c5fb8d6627
Kim Kyeong Hye <87649569+kimkyeonghye@users.noreply.github.com>
2022-09-06 01:19:47 +0900

    Time: 313 ms (29.95%), Space: 51.9 MB (11.43%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,11 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-  if (x < 0) return false;
-  const tmp = x.toString();
-  const middle = tmp.length / 2;
-  for (let i = 0; i < middle; i++) {
-    if (tmp[i] !== tmp[tmp.length - i - 1]) return false;
-  }
-  return true;
+  let tmp = Number(String(x).split('').reverse().join(''));
+  return x === tmp
 };
