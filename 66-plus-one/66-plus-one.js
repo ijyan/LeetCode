4172b6946f6de017de1aebd8b4f2fab084b7cec6
Kim Kyeong Hye <87649569+kimkyeonghye@users.noreply.github.com>
2022-09-08 18:12:04 +0900

    Time: 100 ms (29.45%), Space: 41.8 MB (84.39%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -3,10 +3,7 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-  for (let i = digits.length - 1; i >= 0; i--) {
-    digits[i]++;
-    if (digits[i] > 9) digits[i] = 0;
-    else return digits;
-  }
-  return [1, ...digits];
+  digits = BigInt(digits.join(''));
+  digits++;
+  return digits.toString().split('');
 };
