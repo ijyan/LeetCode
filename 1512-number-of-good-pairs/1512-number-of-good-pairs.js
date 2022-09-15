6705ea7958d57b689f0b8a80b10a5a68e653070b
Kim Kyeong Hye <87649569+kimkyeonghye@users.noreply.github.com>
2022-09-15 11:42:41 +0900

    Time: 87 ms (57.01%), Space: 41.8 MB (69.24%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -4,7 +4,7 @@
  */
 var numIdenticalPairs = function(nums) {
   let cnt = 0;
-  for(let i = 0; i < nums.length; i++) {
+  for(let i = 0; i < nums.length - 1; i++) {
     for(let j = i + 1; j < nums.length; j++) {
       if(nums[i] === nums[j]) cnt++;
     }
