05de1b0eccb0c1fbe69413f83e29474fa65dbc27
Kim Kyeong Hye <87649569+kimkyeonghye@users.noreply.github.com>
2022-09-15 11:30:41 +0900

    Time: 105 ms (22.42%), Space: 42.6 MB (15.08%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.js
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -3,6 +3,5 @@
  * @return {number}
  */
 var maximumWealth = function(accounts) {
-  const answer = accounts.map(v => v.reduce((acc, cur) => acc + cur))
-  return Math.max(...answer);
+  return Math.max(...accounts.map(v => v.reduce((acc, cur) => acc + cur)));
 };
