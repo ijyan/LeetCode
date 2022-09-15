6910a63a223a7a55ce513f637ebd71f783f6d88d
Kim Kyeong Hye <87649569+kimkyeonghye@users.noreply.github.com>
2022-09-15 15:45:13 +0900

    Time: 115 ms (19.60%), Space: 42.5 MB (61.08%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
@@ -4,5 +4,5 @@
  * @return {number}
  */
 var findFinalValue = function(nums, original) {
-  return nums.indexOf(original) === -1 ? original : findFinalValue(nums, original * 2);
+  return nums.includes(original) ? findFinalValue(nums, original * 2) : original;
 };
