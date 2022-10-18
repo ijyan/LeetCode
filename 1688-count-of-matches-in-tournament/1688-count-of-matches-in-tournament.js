5f3a3db45ebebb7b2ea87f32971854ddc3e34456
Kim Kyeong Hye <87649569+kimkyeonghye@users.noreply.github.com>
2022-10-18 16:18:57 +0900

    Time: 86 ms (68.01%), Space: 41.9 MB (69.89%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.js
@@ -3,20 +3,5 @@
  * @return {number}
  */
 const numberOfMatches = (n) => {
-  let matches = 0;
-  while (true) {
-    if (n === 1) break;
-    let advance = tournament(n);
-    matches += n - advance;
-    n = advance;
-  }
-  return matches;
-}
-
-const tournament = (team) => {
-  if (team % 2 === 0) {
-    return team / 2;
-  } else {
-    return (team - 1) / 2 + 1;
-  }
+  return n - 1;
 }
